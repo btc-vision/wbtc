63fe6e662332a62bc498767a6e84625c69e238fa
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-05-16 03:42:13 -0400

    Update MintEvent.ts

--- src/contract/events/MintEvent.ts
+++ src/contract/events/MintEvent.ts
@@ -5,7 +5,7 @@ import { u256 } from 'as-bignum/assembly';
 
 export class MintEvent extends NetEvent {
     constructor(address: Address, amount: u256) {
-        const data: BytesWriter = new BytesWriter();
+        const data: BytesWriter = new BytesWriter(1, true);
         data.writeAddress(address);
         data.writeU256(amount);
 
