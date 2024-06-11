1c97218aa63f44ca8a069edd4995f92d50090398
BlobMaster41 <96896824+BlobMaster41@users.noreply.github.com>
2024-06-10 22:57:02 -0400

    Added max supply

--- src/contract/WBTC.ts
+++ src/contract/WBTC.ts
@@ -3,6 +3,7 @@ import { OP_0 } from '../btc/contracts/OP_0';
 import { Calldata } from '../btc/universal/ABIRegistry';
 import { BytesWriter } from '../btc/buffer/BytesWriter';
 import { Selector } from '../btc/math/abi';
+import { u256 } from 'as-bignum/assembly';
 
 @final
 export class wBTC extends OP_0 {
@@ -12,7 +13,7 @@ export class wBTC extends OP_0 {
     public readonly symbol: string = 'wBTC';
 
     constructor(self: Address, owner: Address) {
-        super(self, owner);
+        super(self, owner, u256.fromBytes([74, 155, 99, 132, 72, 128, 0]));
     }
 
     public callMethod(method: Selector, calldata: Calldata): BytesWriter {
