2b806c1943058854a407393d86260699366f9156
Predmet <175954199+impredmet@users.noreply.github.com>
2024-09-26 08:27:51 -0400

    Refactor StackingOP20 and wBTC contracts

--- src/events/WithdrawalRequestEvent.ts
+++ src/events/WithdrawalRequestEvent.ts
@@ -8,8 +8,7 @@ import { u256 } from 'as-bignum/assembly';
 
 export class WithdrawalRequestEvent extends NetEvent {
     constructor(amount: u256, address: Address) {
-        const length: i32 = ADDRESS_BYTE_LENGTH + 32;
-        const data: BytesWriter = new BytesWriter(length);
+        const data: BytesWriter = new BytesWriter(ADDRESS_BYTE_LENGTH + 32);
         data.writeAddress(address);
         data.writeU256(amount);
 
