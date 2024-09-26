da11d64ed682ae10d182796f088e381418bd5066
Predmet <175954199+impredmet@users.noreply.github.com>
2024-09-26 08:15:05 -0400

    Update wbtc to use the latest btc-runtime version and fix length BytesWriter

--- src/events/WithdrawalRequestEvent.ts
+++ src/events/WithdrawalRequestEvent.ts
@@ -1,9 +1,15 @@
-import { Address, BytesWriter, NetEvent } from '@btc-vision/btc-runtime/runtime';
+import {
+    Address,
+    ADDRESS_BYTE_LENGTH,
+    BytesWriter,
+    NetEvent,
+} from '@btc-vision/btc-runtime/runtime';
 import { u256 } from 'as-bignum/assembly';
 
 export class WithdrawalRequestEvent extends NetEvent {
     constructor(amount: u256, address: Address) {
-        const data: BytesWriter = new BytesWriter();
+        const length: i32 = ADDRESS_BYTE_LENGTH + 32;
+        const data: BytesWriter = new BytesWriter(length);
         data.writeAddress(address);
         data.writeU256(amount);
 
